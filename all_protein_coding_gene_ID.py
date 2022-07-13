fe5bb6c1789effbfd7eecb116d2a741bf1ec5cc3
Christian Bluemel <chrisbl@will.izn-ffm.intern>
2022-07-13 14:34:27 +0200

    fixed datastructure error within the transcript_dict

--- all_protein_coding_gene_ID.py
+++ all_protein_coding_gene_ID.py
@@ -19,23 +19,38 @@ def extract_protein_coding_ids(ensembl_path):
                                                     gtf['transcript_id'],
                                                     gtf['transcript_biotype']) if biotype == "protein_coding"]
     transcript_dict = dict()
-    transcript_list = list()
+    transcript_list = set()
     protein_coding_gene_ids = set()
     for key, transcript_id in protein_coding_ids:
         transcript_dict[key] = []
         protein_coding_gene_ids.add(key)
     for key, transcript_id in protein_coding_ids:
-        transcript_dict[key].append(transcript_id)
-        transcript_list.append(transcript_id)
+        transcript_dict[key].append([transcript_id])
+        transcript_list.add(transcript_id)
     for key in transcript_dict.keys():
         transcript_dict[key] = set(transcript_dict[key])
         transcript_dict[key] = sorted(list(transcript_dict[key]))
+        transcript_dict[key] = [ [entry] for entry in transcript_dict[key]]
     protein_coding_gene_ids = sorted(list(protein_coding_gene_ids))
+    transcript_list = list(transcript_list)
     
     return transcript_dict, transcript_list, protein_coding_gene_ids
     
 def main():
-    extract_protein_coding_ids()
+    transcript_dict, transcript_list, protein_coding_gene_ids = extract_protein_coding_ids("/share/project/zarnack/chrisbl/FAS/utility/protein_lib/Homo_sapiens.GRCh38.107.gtf")
+    print(transcript_list[0:20])
+    count = 0
+    flag = False
+    for key in transcript_dict.keys():
+        count += 1
+        for ID in transcript_dict[key]:
+            if ID == "E":
+                print("KEY IS ", key, "::")
+                print(transcript_dict[key])
+        if flag:
+            break
+                
+                
 
 if __name__ == "__main__":
     main()
