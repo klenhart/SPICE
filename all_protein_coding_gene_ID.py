024d2238d0e2d152104d262c367f1b9060d932c4
Christian Bluemel <chrisbl@will.izn-ffm.intern>
2022-07-26 13:45:52 +0200

    Fused GET APi requests into a single POST request

--- all_protein_coding_gene_ID.py
+++ all_protein_coding_gene_ID.py
@@ -15,44 +15,29 @@ def extract_protein_coding_ids(ensembl_path):
     print("Loading local ensembl dataframe...")
     gtf = pr.read_gtf(ensembl_path, as_df=True)
     print("Extracting IDs of protein coding transcripts...")
-    protein_coding_ids = [ (gene_id, transcript_id) for gene_id,
-                                     transcript_id,
-                                     biotype in zip(gtf['gene_id'],
+    protein_coding_ids = [ (gene_id, transcript_id, protein_id) for gene_id,
+                                      transcript_id,
+                                      protein_id,
+                                      biotype in zip(gtf['gene_id'],
                                                     gtf['transcript_id'],
-                                                    gtf['transcript_biotype']) if biotype == "protein_coding"]
+                                                    gtf['protein_id'],
+                                                    gtf['transcript_biotype']
+                                                    ) if biotype == "protein_coding" and str(type(protein_id)) != "<class 'float'>"]
+
+    protein_coding_ids = list(set([i for i in protein_coding_ids]))
+        
     print("Assembling IDs...")
-    transcript_dict = dict()
-    transcript_list = set()
-    protein_coding_gene_ids = set()
-    for key, transcript_id in protein_coding_ids:
-        transcript_dict[key] = []
-        protein_coding_gene_ids.add(key)
-    for key, transcript_id in protein_coding_ids:
-        transcript_dict[key].append(transcript_id)
-        transcript_list.add(transcript_id)
-    print("Sorting IDs...")
-    for key in transcript_dict.keys():
-        transcript_dict[key] = set(transcript_dict[key])
-        transcript_dict[key] = sorted(list(transcript_dict[key]))
-    protein_coding_gene_ids = sorted(list(protein_coding_gene_ids))
-    transcript_list = list(transcript_list)
-    
-    return transcript_dict, transcript_list, protein_coding_gene_ids
+    protein_coding_ids.sort()
+    return protein_coding_ids
     
 def main():
-    transcript_dict, transcript_list, protein_coding_gene_ids = extract_protein_coding_ids("/share/project/zarnack/chrisbl/FAS/utility/protein_lib/FAS_library/Homo_sapiens.GRCh38.107.gtf")
-    print(transcript_list[0:20])
-    count = 0
-    flag = False
-    for key in transcript_dict.keys():
-        count += 1
-        for ID in transcript_dict[key]:
-            if ID == "E":
-                print("KEY IS ", key, "::")
-                print(transcript_dict[key])
-        if flag:
-            break
-                
+    prefix = "/share/project/zarnack/chrisbl/FAS/utility/protein_lib/FAS_library/"
+    ensembl_path1 = "Homo_sapiens.GRCh38.107.gtf"
+    ensembl_path2 = "Tetraodon_nigroviridis.TETRAODON8.107.gtf"
+    
+    protein_coding_ids = extract_protein_coding_ids(prefix + ensembl_path2)
+    #print(protein_coding_ids[0:20])
+
                 
 
 if __name__ == "__main__":
--- ensembl_access.py
+++ ensembl_access.py
@@ -26,35 +26,49 @@ from healthcheck import healthcheck
 
 # /share/project/zarnack/chrisbl/FAS/utility/protein_lib
 
+def ping_ensembl():
+    import requests, sys
+ 
+    server = "https://rest.ensembl.org"
+    ext = "/info/ping?"
+ 
+    r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
+ 
+    if not r.ok:
+        r.raise_for_status()
+        sys.exit()
+ 
+    decoded = r.json()
+    return bool(decoded["ping"])
 
-def assemble_protein_seqs(transcript_dict, assembly_num, species, library_path, root_path):
+def make_request_data(id_list):
+    request_data = '{ "ids" : ['
+    for entry in id_list:
+        request_data += '"' + entry + '", '
+    request_data = request_data[:-2]
+    request_data += ' ] }'
+    return request_data
+
+def assemble_protein_seqs(protein_coding_ids, assembly_num, species, library_path, root_path):
     """
 
     Parameters
     ----------
-    dictionary with gene IDs as keys and transcript IDs in a list of one in another list.
-        {
-            gene_id_1 : [[transcript_id_1], [transcript_id_2]],
-            gene_id_2 : [[transcript_id_3], [transcript_id_4]],
+    list with gene, transcript and protein IDs as tuples
+        [
+            (gene_id_1, transcript_id_1, protein_id_1),
+            (gene_id_1, transcript_id_2, protein_id_2),
+            (gene_id_2, transcript_id_3, protein_id_3),
             .
             .
             .
             ...etc...
             
-            }
+            ]
 
     Returns
     -------
-    library_dict with gene IDs as keys and transcript IDs combined with protein sequences in a list of two.
-        {
-            gene_id_1 : [[transcript_id_1, seq1], [transcript_id_2, seq2]],
-            gene_id_2 : [[transcript_id_3, seq3], [transcript_id_4, seq4]],
-            .
-            .
-            .
-            ...etc...
-            
-            }
+    counts on how many request were found, not found and how many genes were assembled.
     """
     if not os.path.exists(root_path):
         os.makedirs(root_path)
@@ -68,30 +82,42 @@ def assemble_protein_seqs(transcript_dict, assembly_num, species, library_path, 
     
     count_not_found = 0
     count_found = 0
-    count_genes = 0
+    gene_ids = [gene_id for gene_id, transcript_id, protein_id in protein_coding_ids]
+    count_genes = len(list(set(gene_ids)))
     
-    url_prefix = "https://rest.ensembl.org/sequence/id/"
-    url_suffix = "?object_type=transcript;type=protein;species=" + species + ";"
-    headers = { "Content-Type" : "application/json"}
+    server = "https://rest.ensembl.org/sequence/id"
+    headers={ "Content-Type" : "application/json", "Accept" : "application/json"}
     
-    print(len(transcript_dict.keys()), " protein coding genes prepared for ensembl sequence requests...")
+    protein_ids = [protein_id for gene_id, transcript_id, protein_id in protein_coding_ids]
+    request_data = make_request_data(protein_ids)
+    
+    for x in range(3):
+        r = requests.post(server, headers=headers, data=request_data)
+        if r.ok:
+            break
+        elif x > 1:
+            print("Failed to request sequenes 3 times. Checking if ensembl service is up.")
+            if not ping_ensembl():
+                print("Ensembl is currently down. Can't download sequences.")
+            else:
+                print("Ensembl is up. Can't identify error source.")
+            r.raise_for_status()
+            sys.exit()
         
-    for key in transcript_dict.keys():
-        count_genes += 1
-        for i, transcript_id in enumerate(transcript_dict[key]):
-                for x in range(3):
-                    r = requests.get(url_prefix + transcript_id + url_suffix, headers=headers)
-                    if r.ok:
-                        count_found += 1
-                        seq = r.json()["seq"]
-                        data = ">" + key + "|" + transcript_id + "|species:" + species + "|assembly:" + str(assembly_num) + "\n" + seq + "\n"
-                        with open(isoforms_path, "a") as fasta:
-                            fasta.write(data)
-                        break
-                    elif x == 2:
-                        count_not_found += 1
-                        with open(not_found_path, "a") as f:
-                            f.write(key + "|" + transcript_id + "\n")
+    decoded = r.json()
+    for i, entry in protein_coding_ids:
+        count_found += 1
+        seq = decoded[i]["seq"]
+        gene_id, transcript_id, protein_id = entry
+        if seq == "":
+            count_not_found += 1
+            with open(not_found_path, "a") as fasta:
+                fasta.write(gene_id + " " + transcript_id + " " + protein_id + "\n")
+        else:
+            data = ">" + gene_id + "|" + transcript_id + "|" + protein_id + "|species:" + species + "|assembly:" + str(assembly_num) + "\n" + seq + "\n"
+            with open(isoforms_path, "a") as fasta:
+                fasta.write(data)
+
     return count_found, count_not_found, count_genes
 
 def parser_setup():
@@ -108,7 +134,7 @@ def parser_setup():
     parser = argparse.ArgumentParser()
     
     parser.add_argument("-o", "--output", type=str,
-                        help=""""Specify location of the library. FAS_library folder can already exist in this folder.
+                        help="""Specify location of the library. FAS_library folder can already exist in this folder.
                         If creating the library, this folder should contain the local ensembl assembly. If it does not,
                         you can download the local ensembl assembly using the -l (--local) argument. It will then be automatically
                         download into the FAS_library folder within the folder given in this argument.""")
@@ -167,16 +193,17 @@ def main():
         if not os.path.isfile(ensembl_path):
             print(ensembl_path, "does not exist. Maybe you have an old release of the local ensembl GTF. You can download a current one for your species by using the -l argument additional to your currently used arguments.")
             sys.exit()
-        transcript_dict, transcript_id_list, gene_id_list = extract_protein_coding_ids(ensembl_path)
+        protein_coding_ids = extract_protein_coding_ids(ensembl_path)
         if not os.path.exists(library_path):
             os.makedirs(library_path)
         root_path = make_rootpath(library_path, species, release_num) 
-        count_found, count_not_found, count_genes = assemble_protein_seqs(transcript_dict, release_num, species, library_path, root_path)
+        count_found, count_not_found, count_genes = assemble_protein_seqs(protein_coding_ids, release_num, species, library_path, root_path)
         print("Saved isoforms as fasta in", root_path + "/isoforms.fasta")
-        print(count_genes, "protein coding genes processed...")
+        print(count_genes, "protein coding genes processed.")
         print(count_found, "protein sequences integrated into library assembled.")
-        print(count_not_found, "protein sequences not found. IDs written into", root_path + "not_found.txt...")
+        print(count_not_found, "protein sequences not found. IDs written into", root_path + "not_found.txt.")
         print("Library assembly complete.")
 
 if __name__ == "__main__":
     main()
+
