6caa2e8920f90d34c8532c77800550ef74b6e5ed
chrisbluemel <s5301686@stud.uni-frankfurt.de>
2022-11-28 17:26:43 +0100

    fixed join misuse

--- fas_compare.py
+++ fas_compare.py
@@ -61,7 +61,7 @@ def generate_comparison(fas_ring_dict_list, conditions, fas_mode, result_config_
     file_path = comparison_path + "_".join(["result", "@".join(conditions), fas_mode]) + ".tsv"
 
     fas_ring_dict1, fas_ring_dict2 = fas_ring_dict_list
-    output = "\n".join("!conditions " + conditions[0] + " " + conditions[1],
+    output = "\n".join(["!conditions " + conditions[0] + " " + conditions[1],
                        "!FASmode " + fas_mode,
                        "!ResultOrigin " + result_config_dict["result_dir"],
                        "\t".join("gene_id",
@@ -76,7 +76,7 @@ def generate_comparison(fas_ring_dict_list, conditions, fas_mode, result_config_
                                  "2rmsd_smaller_max",
                                  "2rmsd_smaller_mean_plus_std",
                                  "rmsd",
-                                 "max_tsl"))
+                                 "max_tsl")])
     
     for gene_id in fas_ring_dict1.keys():
         ring_list = [ fas_ring_dict1[gene_id], fas_ring_dict2[gene_id]]
