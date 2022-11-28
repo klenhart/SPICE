81184d3853d153e61a8680facd30730dfe5851f9
chrisbluemel <s5301686@stud.uni-frankfurt.de>
2022-11-28 20:51:41 +0100

    fixed an indexing bug

--- fas_compare.py
+++ fas_compare.py
@@ -31,6 +31,8 @@ import argparse
 import os
 import json
 import sys
+import time
+import random
 
 import valves.fas_polygon as poly
 import valves.library_class as library_class
@@ -53,6 +55,87 @@ def extract_all_graph(fas_lib, movement_dict, exempt=["ENSG00000155657"]):
                                       movement_dict["movement"][gene_id]["minus_std_mov"],
                                       movement_dict["movement"][gene_id]["intersample_rmsd_mean"]]
     return fas_ring_dict
+
+
+def prepare_ring_pair_visual(ring_list, gene_id, conditions):
+    protein_ids = []
+    mean_movs = []
+    mean_rel_exprs = []
+    min_movs = []
+    max_movs = []
+    plus_std_movs = []
+    minus_std_movs = []
+    intersample_rmsd = []
+
+    for prot_ids_list, mean_mov_list, mean_rel_expr_list, min_mov_list, max_mov_list, plus_std_mov_list, minus_std_mov_list, intersample_rmsd_mean in ring_list:
+        protein_ids = prot_ids_list
+        mean_movs.append(mean_mov_list)
+        mean_rel_exprs.append(mean_rel_expr_list)
+        min_movs.append(min_mov_list)
+        max_movs.append(max_mov_list)
+        plus_std_movs.append(plus_std_mov_list)
+        minus_std_movs.append(minus_std_mov_list)
+        intersample_rmsd.append(intersample_rmsd_mean)
+
+    
+    delete_list = []
+    for i, mean_rel_expr in enumerate(mean_rel_exprs):
+        for j, _ in enumerate(mean_rel_expr):
+            if all([ entry[j] == 0 for entry in mean_rel_exprs ]):
+                delete_list.append(j)
+        break
+    
+    categories = []
+    final_mean_movs = []
+    final_min_movs = []
+    final_max_movs = []
+    final_plus_std_movs = []
+    final_minus_std_movs = []
+    
+    for i in range(2):
+        final_mean_movs.append([])
+        final_min_movs.append([])
+        final_max_movs.append([])
+        final_plus_std_movs.append([])
+        final_minus_std_movs.append([])
+    for i, prot_id in enumerate(protein_ids):
+        if i not in delete_list:
+            categories.append(prot_id)
+            for j in range(2):
+                final_mean_movs[j].append(mean_movs[j][i])
+                final_min_movs[j].append(min_movs[j][i])
+                final_max_movs[j].append(max_movs[j][i])
+                final_plus_std_movs[j].append(plus_std_movs[j][i])
+                final_minus_std_movs[j].append(minus_std_movs[j][i])
+
+    rmsd = fas_utility.calc_rmsd(final_mean_movs)
+    
+    rmsd_max = fas_utility.calc_rmsd(final_max_movs)
+    rmsd_plus_std = fas_utility.calc_rmsd(final_plus_std_movs)
+
+    rmsd_max_smaller_1 = False if rmsd_max < intersample_rmsd[0] else True
+    rmsd_mean_plus_std_smaller_1 = False if rmsd_plus_std < intersample_rmsd[0] else True
+    rmsd_max_smaller_2 = False if rmsd_max < intersample_rmsd[1] else True
+    rmsd_mean_plus_std_smaller_2 = False if rmsd_plus_std < intersample_rmsd[1] else True
+       
+    
+    output_dict = dict()
+    output_dict["gene_id"] = gene_id
+    output_dict["categories"] = categories
+    output_dict["mean_movement"] = final_mean_movs
+    output_dict["min_movement"] = final_min_movs
+    output_dict["max_movement"] = final_max_movs
+    output_dict["plus_std_movement"] = final_plus_std_movs
+    output_dict["minus_std_movement"] = final_minus_std_movs
+    
+    output_dict["rmsd_max_smaller_1"] = str(int(rmsd_max_smaller_1))
+    output_dict["rmsd_mean_plus_std_smaller_1"] = str(int(rmsd_mean_plus_std_smaller_1))
+    output_dict["rmsd_max_smaller_2"] = str(int(rmsd_max_smaller_2))
+    output_dict["rmsd_mean_plus_std_smaller_2"] = str(int(rmsd_mean_plus_std_smaller_2))
+        
+    output_dict["rmsd"] = rmsd
+    
+    return output_dict
 
 
 def generate_comparison(fas_ring_dict_list, conditions, fas_mode, result_config_dict, fas_lib, movement_dict_list, expression_dict_list, movement_paths, expression_paths):
@@ -81,7 +164,7 @@ def generate_comparison(fas_ring_dict_list, conditions, fas_mode, result_config_
     for gene_id in fas_ring_dict1.keys():
         ring_list = [ fas_ring_dict1[gene_id], fas_ring_dict2[gene_id]]
         
-        fas_ring_dict = poly.prepare_ring_pair_visual(ring_list, gene_id, conditions)
+        fas_ring_dict = prepare_ring_pair_visual(ring_list, gene_id, conditions)
         max_tsl = fas_utility.find_max_tsl(fas_lib, fas_ring_dict["categories"])
         
         output_row_list = [ fas_ring_dict["gene_id"] ]
@@ -104,46 +187,6 @@ def generate_comparison(fas_ring_dict_list, conditions, fas_mode, result_config_
     with open(file_path, "w") as f:
         f.write(output)
 
-"""// FOR NOW NOT NECESSARY ANYMORE SINCE JULIAN IS BUILDING THE GUI
-def sort_by_rmsd(fas_lib, path, flag_more_than_2=True):
-    output = "gene_id\tprot_id\t1_in_2_std\t2_in_1_std\tin_std_sum\trmsd\tmax_tsl\n"
-    new_path = path[:-4] + "_sorted.tsv"
-    with open(path, "r") as f:
-        file = f.read()
-        comparisons = file.split("\n")
-    comparisons = comparisons[4:]
-    output = "\n".join(comparisons[0],
-                       comparisons[1],
-                       comparisons[2],
-                       "\t".join("gene_id",
-                                 "prot_id",
-                                 "1_in_2_std",
-                                 "2_in_1_std",
-                                 "in_std_sum",
-                                 "rmsd",
-                                 "max_tsl"))
-    comparisons = [ comparison.split("\t") for comparison in comparisons ]
-    new_comparisons = []
-    for comparison in comparisons:
-        if len(comparison) == 12:
-            new_comparisons.append(comparison)
-    comparisons = new_comparisons
-    comparisons = [ comparison[:2] + comparison[7:9] + [int(comparison[-3]),
-                                                        float(comparison[-2]),
-                                                        int(comparison[-1])] for comparison in comparisons ]
-    new_comparisons = []
-    for comparison in comparisons:
-        if comparison[-2] < 1:
-            new_comparisons.append(comparison)
-    comparisons = new_comparisons
-    comparisons = sorted(comparisons, key = lambda x: (x[-1], -x[-2], -x[3]))
-    comparisons = [ comparison[:2] + [str(comparison[-2]), str(comparison[-1])] for comparison in comparisons ]
-    comparisons = [ "\t".join(comparison) for comparison in comparisons ]
-    file = "\n".join(comparisons)
-    file = output + file
-    with open(new_path, "w") as f:
-        f.write(file)
-"""
 
 def parser_setup():
     """
@@ -188,7 +231,7 @@ def parser_setup():
     
 def main():
     config_path, conditions, result_config_path, flag_lcr, flag_tmhmm, flag_all = parser_setup()
-
+    time.sleep(random.randint(10, 50))
     conditions = sorted(conditions)
 
     if flag_lcr:
