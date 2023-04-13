16c15395193b42adc65ab6248e8ab97d565e56f8
chrisbluemel <s5301686@stud.uni-frankfurt.de>
2023-04-13 13:01:46 +0200

    Wrote the basic script for the FASJobAssistant. Current problems are, if I need to generate a temporary file containing all gene IDs to be compared.

--- Classes/FASJobAssistant/FASJobAssistant.py
+++ Classes/FASJobAssistant/FASJobAssistant.py
@@ -1,4 +1,8 @@
 #!/bin/env python
+import os
+
+from Classes.PassPath.PassPath import PassPath
+
 
 #######################################################################
 # Copyright (C) 2023 Christian Bluemel
@@ -19,3 +23,42 @@
 #  along with Spice.  If not, see <http://www.gnu.org/licenses/>.
 #
 #######################################################################
+
+RAW_SCRIPT = """#!/bin/bash
+#SBATCH --partition={14}
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu={15}G
+#SBATCH --job-name="fas_{4}{12}"
+#SBATCH --output=/dev/null 
+#SBATCH --error=/dev/null
+#SBATCH --array={10}-{11}
+gene=$(awk FNR==$SLURM_ARRAY_TASK_ID "{13}gene_ids{12}.txt")
+{0} {1} \
+-m \
+-g $gene \
+-c {2} \
+&& \
+{3} \
+--seed {5} \
+--query {5} \
+--annotation_dir {6} \
+--out_dir {7} \
+--bidirectional \
+--pairwise {8}$gene.tsv \
+--out_name $gene \
+--tsv \
+--phyloprofile {9} \
+--empty_as_1 \
+"""
+
+RAW_SLURM_2 = """; \
+{0} {1} \
+-r \
+-g $gene \
+-c {2}"""
+
+class FASJobAssistant:
+
+    def __init__(self, pass_path: PassPath, fas_dir: str):
+        self.fas_anno: str = os.path.join(fas_dir, "fas.doAnno")
+        self.fas_run: str = os.path.join(fas_dir, "fas.run")
