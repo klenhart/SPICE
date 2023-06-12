88ae1deff8c0e1670c1540b19fc9c990f55f3e0a
chrisbluemel <s5301686@stud.uni-frankfurt.de>
2023-06-12 13:14:38 +0200

    Finished extracting the the novel transcripts, now I just have to save them in GTF format to pipe them into Transdecoder.

--- AnnotationParser.py
+++ AnnotationParser.py
@@ -34,25 +34,44 @@ class AnnotationParser:
     def parse_annotations(self):
         for annotation_path in self.annotation_path_list:
             gtf_iterator: GTFBoy = GTFBoy(annotation_path)
+            count = 0
             for line in gtf_iterator:
                 if not line.startswith("#"):
                     line_dict: Dict[str, str] = GTFBoy.build_dict(line.split("\t"))
                     line_dict["gene_id"] = line_dict["gene_id"].split(".")[0]
+                    if line_dict["feature"] == "exon":
+                        line_dict["exon_id"] = line_dict["exon_id"].split(".")[0]
                     if AnnotationParser.check_if_candidate(line_dict):
+                        count += 1
                         gene_id: str = line_dict["gene_id"]
                         transcript_id: str = line_dict["transcript_id"]
                         feature: str = line_dict["feature"]
-                        if gene_id in self.transcript_dict.keys():
-                            pass
-                        else:
+                        if gene_id not in self.transcript_dict.keys():
                             self.transcript_dict[gene_id] = dict()
+                        if transcript_id not in self.transcript_dict[gene_id].keys():
                             self.transcript_dict[gene_id][transcript_id] = dict()
-                            if feature == "exon":
-                                exon_id: str = line_dict["exon_id"]
-                                self.transcript_dict[gene_id][transcript_id][exon_id] = line_dict
-                        print(line_dict)
-                        print(GTFBoy.line_dict_to_gtf_line(line_dict))
-                        break
+
+                        gtf_line = GTFBoy.line_dict_to_gtf_line(line_dict)
+                        if feature == "exon":
+                            exon_id: str = line_dict["exon_id"]
+                            self.transcript_dict[gene_id][transcript_id][exon_id] = gtf_line
+                        else:
+                            self.transcript_dict[gene_id][transcript_id]["GTF_line"] = gtf_line
+                if count >= 20:
+                    count = 0
+                    break
+        print(self)
+
+    def __str__(self) -> str:
+        output_string: str = ""
+        for gene_id in self.transcript_dict.keys():
+            output_string += gene_id + "\n"
+            for transcript_id in self.transcript_dict[gene_id].keys():
+                output_string += "\t" + transcript_id + "\n"
+                for exon_id in self.transcript_dict[gene_id][transcript_id].keys():
+                    output_string += "\t\t" + exon_id + "\n"
+                    output_string += "\t\t\t" + self.transcript_dict[gene_id][transcript_id][exon_id] + "\n"
+        return output_string
 
     def save(self, out_path: str):
         pass
