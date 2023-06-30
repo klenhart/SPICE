69ce76238ef17e592b52afef401c538a802a9fa2
chrisbluemel <s5301686@stud.uni-frankfurt.de>
2023-06-30 22:58:20 +0200

    Optimized FAS final since the adjustment to split up fas score files was incredibly slow due to doing tons of IO. Now it parses the phyloprofile file entirely first and then loads the entire fused library in the geneAssembler and that adds the new scores in order.

--- FASResultHandler.py
+++ FASResultHandler.py
@@ -3,7 +3,10 @@ import json
 import os
 from typing import Dict, Any, List
 
+from Classes.PassPath.PassPath import PassPath
 from Classes.ReduxArgParse.ReduxArgParse import ReduxArgParse
+from Classes.SequenceHandling.GeneAssembler import GeneAssembler
+from Classes.SequenceHandling.LibraryInfo import LibraryInfo
 from Classes.WriteGuard.WriteGuard import WriteGuard
 
 
@@ -74,31 +77,46 @@ def main():
                 with open(os.path.join(argument_dict["anno_dir"], "reverse.domains"), "a") as f_out:
                     f_out.write(domains_input)
     elif argument_dict['mode'] == "integrate":
-        with WriteGuard(os.path.join(argument_dict["anno_dir"], "fas_index.json"), argument_dict["anno_dir"]):
-            fas_scores_dir: str = os.path.join(argument_dict["anno_dir"], "fas_scores")
-            with open(os.path.join(argument_dict["anno_dir"], "fas_index.json"), "r") as f_in:
-                fas_index_dict: Dict[str, str] = json.load(f_in)
-            with open(os.path.join(argument_dict["anno_dir"], "fas.phyloprofile")) as f_in:
-                fas_score_list: List[str] = f_in.read().split("\n")[1:]
-            for line in fas_score_list:
-                if len(line) == 0:
-                    continue
-                split_line: List[str] = line.split("\t")
-                seed: str = split_line[0]
-                query: str = split_line[2]
-                fas_1: float = float(split_line[3])
-                fas_2: float = float(split_line[4])
-                split_seed: List[str] = seed.split("|")
-                split_query: List[str] = query.split("|")
-                gene_id: str = split_seed[0]
-                seed_prot_id: str = split_seed[1]
-                query_prot_id: str = split_query[1]
-                with open(os.path.join(fas_scores_dir, fas_index_dict[gene_id]), "r") as f:
-                    fas_scores_dict = json.load(f)
-                fas_scores_dict[gene_id][seed_prot_id][query_prot_id] = fas_2
-                fas_scores_dict[gene_id][query_prot_id][seed_prot_id] = fas_1
-                with open(os.path.join(fas_scores_dir, fas_index_dict[gene_id]), "w") as f:
-                    json.dump(fas_scores_dict, f, indent=4)
+        lib_path_dir = os.path.join("/".join(argument_dict["anno_dir"].split("/")[:-1]), "paths.json")
+        with open(lib_path_dir, "r") as f:
+            path_dict = json.load(f)
+        pass_path: PassPath = PassPath(path_dict)
+        lib_info: LibraryInfo = LibraryInfo(pass_path["info"])
+        gene_assembler: GeneAssembler = GeneAssembler(lib_info["info"]["species"],
+                                                      str(lib_info["info"]["taxon_id"]))
+        gene_assembler.load(pass_path)
+        with open(os.path.join(argument_dict["anno_dir"], "fas.phyloprofile")) as f_in:
+            fas_score_list: List[str] = f_in.read().split("\n")[1:]
+        fas_scores_dict: Dict[str, Dict[str, Dict[str, float]]] = dict()
+        for line in fas_score_list:
+            if len(line) == 0:
+                continue
+            split_line: List[str] = line.split("\t")
+            seed: str = split_line[0]
+            query: str = split_line[2]
+            fas_1: float = float(split_line[3])
+            fas_2: float = float(split_line[4])
+            split_seed: List[str] = seed.split("|")
+            split_query: List[str] = query.split("|")
+            gene_id: str = split_seed[0]
+            seed_prot_id: str = split_seed[1]
+            query_prot_id: str = split_query[1]
+            if gene_id not in fas_scores_dict.keys():
+                fas_scores_dict[gene_id] = dict()
+            if seed_prot_id not in fas_scores_dict[gene_id].keys():
+                fas_scores_dict[gene_id][seed_prot_id] = dict()
+            if query_prot_id not in fas_scores_dict[gene_id].keys():
+                fas_scores_dict[gene_id][query_prot_id] = dict()
+            fas_scores_dict[gene_id][seed_prot_id][query_prot_id] = fas_2
+            fas_scores_dict[gene_id][query_prot_id][seed_prot_id] = fas_1
+
+        for gene in gene_assembler.get_genes():
+            if gene.get_id() in fas_scores_dict.keys():
+                for prot_id_1 in fas_scores_dict[gene.get_id()].keys():
+                    for prot_id_2 in fas_scores_dict[gene.get_id()][prot_id_1].keys():
+                        gene.get_fas_dict()[prot_id_1][prot_id_2] = fas_scores_dict[gene.get_id()][prot_id_1][prot_id_2]
+
+        gene_assembler.save_fas(pass_path)
 
 
 if __name__ == "__main__":
