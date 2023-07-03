ac6adf61630f8dcffa0f925c2b1f93b4ac313139
chrisbluemel <s5301686@stud.uni-frankfurt.de>
2023-07-03 10:31:50 +0200

    Reduced the test main to a simple Hello World again!

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import List, Dict, Any
 
 from Classes.PassPath.PassPath import PassPath
@@ -8,29 +9,7 @@ from spice_result import expression
 
 
 def main():
-
-
-    with open("C:/Users/chris/Desktop/full_test_files/spice_lib_homo_sapiens_94_147/paths.json", "r") as f:
-        paths_dict = json.load(f)
-    pass_path: PassPath = PassPath(paths_dict)
-    gene_assembler: GeneAssembler = GeneAssembler("homo_sapiens",
-                                                  "9606")
-
-    gene_assembler.load(pass_path)
-
-    for gene in gene_assembler.get_genes():
-        delete_list = list()
-        for entry in gene.get_fas_dict().keys():
-            if not entry.startswith("ENS"):
-                delete_list.append(entry)
-        for entry in delete_list:
-            del gene.get_fas_dict()[entry]
-        for entry in gene.get_fas_dict().keys():
-            for del_entry in delete_list:
-                del gene.get_fas_dict()[entry][del_entry]
-
-    gene_assembler.save_fas(pass_path)
-
+    print("Hello World!")
 
 
 def open_json(path) -> Dict[str, Any]:
